821c320495888db2dd5be50d9c66179b0bed983b
Erel E. Ropeta-Japco <44773736+ereljapco@users.noreply.github.com>
2023-02-13 09:53:39 +0800

    style: fix hover fill of the theme button icons

--- src/layouts/Header/ThemeBtn.jsx
+++ src/layouts/Header/ThemeBtn.jsx
@@ -1,7 +1,15 @@
+import { ReactComponent as IconSun } from '../../assets/icons/icon-sun.svg';
+import { ReactComponent as IconMoon } from '../../assets/icons/icon-moon.svg';
+
 function ThemeBtn({ theme, icon, alt }) {
   return (
     <button className="header__theme-btn">
-      {theme} <img src={icon} alt={alt} />
+      {theme}{' '}
+      {theme === 'dark' ? (
+        <IconMoon className="header__theme-btn-icon" />
+      ) : (
+        <IconSun className="header__theme-btn-icon" />
+      )}
     </button>
   );
 }
